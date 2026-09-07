b0e27fa24656e906491bc6b63c19a78581d296cd
KarelBauters <karel.bauters@flandersmake.be>
2026-09-07 16:20:02 +0200

    Add basename to BrowserRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from "./auth";
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/FMA-MockUp/">
         <App />
       </BrowserRouter>
     </AuthProvider>
