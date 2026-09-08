e6f86204866f51a496520f4853a45dce3eabbb3d
KarelBauters <karel.bauters@flandersmake.be>
2026-09-08 09:46:40 +0200

    Fix logo path to use Vite's BASE_URL instead of process.env.PUBLIC_URL

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="brand">
-        <img src={`${process.env.PUBLIC_URL}/logo.jpeg`} alt="logo missing" className="logo" />
+        <img src={`${import.meta.env.BASE_URL}logo.jpeg`} alt="logo missing" className="logo" />
         <Link to="/"><h1>FMAcademy</h1></Link>
       </div>
       <nav>
