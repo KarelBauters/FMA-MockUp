d928c453bac70dbe1318e88d48e574ac8c8c3d40
KarelBauters <karel.bauters@flandersmake.be>
2026-09-08 09:40:20 +0200

    Fix logo image source in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="brand">
-        <img src="/logo.jpeg.png" alt="logo missing" className="logo" />
+        <img src="/logo.jpeg" alt="logo missing" className="logo" />
         <Link to="/"><h1>FMAcademy</h1></Link>
       </div>
       <nav>
